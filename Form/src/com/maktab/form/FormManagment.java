f3853f214243991f36e5ac882090ee5243996087
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-13 18:54:22 +0330

    Add 'scanner' to class FormManagment

--- Form/src/com/maktab/form/FormManagment.java
+++ Form/src/com/maktab/form/FormManagment.java
@@ -6,8 +6,12 @@ public class FormManagment {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         FormLetterWriter form=new FormLetterWriter();
-        form.setFirstName("Ali");
-        form.setLastName("Ahmadi");
+        System.out.println("Enter your first name: ");
+        String firstName=scanner.nextLine();
+        form.setFirstName(firstName);
+        System.out.println("Enter your last name: ");
+        String lastName=scanner.nextLine();
+        form.setLastName(lastName);
         form.displaySalutation(form.getFirstName(),form.getLastName());
         form.displaySalutation(form.getFirstName());
     }
