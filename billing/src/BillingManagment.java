88ac33c92efde15037af1ca02405f0995042832f
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-13 19:22:13 +0330

    add print line to class 'BillingManagment'

--- billing/src/BillingManagment.java
+++ billing/src/BillingManagment.java
@@ -4,6 +4,12 @@ public class BillingManagment {
     public static void main(String[] args) {
         Billing bill=new Billing();
         Scanner scanner=new Scanner(System.in);
-        System.out.println(bill.computeBill(5,3,2));
+        System.out.println("Enter your book price: ");
+        double bookPrice=scanner.nextDouble();
+        System.out.println("Enter quantity of book: ");
+        int quantity=scanner.nextInt();
+        System.out.println("Enter your coupon: ");
+        int coupon=scanner.nextInt();
+        System.out.format("You must pay:%3.2f ",bill.computeBill(bookPrice,quantity,coupon));
     }
 }
