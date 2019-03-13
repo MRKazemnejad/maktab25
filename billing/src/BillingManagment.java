482a73f7ee7839c55dbd1ccc055ba34918b8951d
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-13 19:09:15 +0330

    Add 'Scanner' to class BillingManagment

--- billing/src/BillingManagment.java
+++ billing/src/BillingManagment.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class BillingManagment {
     public static void main(String[] args) {
         Billing bill=new Billing();
+        Scanner scanner=new Scanner(System.in);
         System.out.println(bill.computeBill(5,3,2));
     }
 }
